dde67a5fc98d8cba315d3fe9a990b1408e783658
GeorgeMonS <jorge.moncada.sosa@gmail.com>
2021-12-12 17:46:41 -0500

    version 1.0.12 cambios en formulario, email

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -16,6 +16,9 @@ export class FormComponent implements OnInit {
   form: FormGroup = new FormGroup({
     FirstName: new FormControl(''),
     LastName: new FormControl(''),
+    DocumentId: new FormControl(''),
+    Cellphone: new FormControl(''),
+    Address: new FormControl(''),
     Email: new FormControl(''),
     Password: new FormControl(''),
     confirmPassword: new FormControl(''),
@@ -29,6 +32,9 @@ export class FormComponent implements OnInit {
     this.form = this.formBuilder.group({
       FirstName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(16)]],
       LastName: ['', Validators.required],
+      DocumentId: ['', Validators.required],
+      Cellphone: ['', [Validators.required, Validators.minLength(7)]],
+      Address: ['', Validators.required],
       Email: ['', [Validators.required, Validators.email]],
       Password: ['', [Validators.required, Validators.minLength(8)]],
       confirmPassword: ['', Validators.required],
